98d9a18911dea88bf5b241efd8fc04b9a42a0f07
Pongsak Prabparn <pongsak@Pongsaks-MacBook-Pro.local>
2018-08-21 13:24:46 +0700

    [*] index.php : edit for k8s

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
     <center>
         <h1>Welcome to Nginx Server !!!</h1>
-        <h2>Your nginx container is working fine.</h2>
+        <h2>Your nginx container is working fine on K8S</h2>
     </center>
     <?php
         phpinfo();
